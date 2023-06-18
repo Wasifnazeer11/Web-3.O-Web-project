1f668650cb8d82eb0f90aff02741a5f5de3b263e
Wasif Nazeer <wasifnazeer11@gmail.com>
2023-06-18 20:46:09 +0500

    first commit

---
+++ client/src/components/Looder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Looder = () => {
+  return (
+    <div>Looder</div>
+  )
+}
+
+export default Looder
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Looder } from './Looder'
+export { default as Navbar } from './navbar' 
+export { default as Footer } from './Footer'
+export { default as Services } from './Services'
+export { default as Transactions } from './Transactions'
+export { default as Welcome } from './Welcome'
+
